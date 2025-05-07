e3a2beaeda30bc27102b07f2794545177e2e2f9c
Damien Cuenot <11711525+dcuenot@users.noreply.github.com>
2025-05-08 00:45:40 +0200

    fix the script

--- scrape_tournaments.py
+++ scrape_tournaments.py
@@ -2,19 +2,24 @@ import requests
 import pandas as pd
 from datetime import datetime
 import os
+from pprint import pprint
 
 def scrape_tournaments():
-    url = "https://monclub.fftt.com/tournaments/search"
+    url = "https://apiv2.fftt.com/api/tournament_requests"
     params = {
         "page": 1,
-        "itemsPerPage": 25,
+        "itemsPerPage": 200,
         "order[startDate]": "asc",
-        "startDate[after]": "2025-05-07T23:59:32",
-        "endDate[before]": "2025-12-31T23:59:59"
+        "startDate[after]": "2025-05-07T00:00:00",
+        "endDate[before]": "2025-12-31T00:00:00"
+    }
+    
+    headers = {
+        'Referer': 'https://monclub.fftt.com/'
     }
     
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
         
         # Parse the JSON response
